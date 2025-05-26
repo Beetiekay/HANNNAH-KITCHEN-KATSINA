3bfb632c134ac603ac1dca1c5ba4eafbf0ee1798
bishir tukur <bastukur1997@gmail.com>
2025-05-26 16:25:22 +0100

    first

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Home from './pages/Home'
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Landing from './pages/Landing'
+import Nav from './components/nav'
 
 export default function App() {
   return (
     <BrowserRouter>
+     <Nav/>
      <Routes>
         <Route path="/" element={<Landing/>}/>
      </Routes>
